f68c090a5c1f15f05fb62e39cbb6b4e59bd9c186
Nelvi975 <nelvi_jimenez@hotmail.com>
2021-10-24 22:25:38 -0500

    segunod commit

---
+++ app/src/main/java/com/example/sistemadelivery/activuty/introActivity.kt
@@ -0,0 +1,41 @@
+package com.example.sistemadelivery.activuty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.BoringLayout
+import com.example.sistemadelivery.R
+import com.google.android.filament.View
+
+
+public class introActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+         startBtn
+        startBtn.findViewById(R.id.startBtn);
+        var new = null
+        with(
+            startBtn,
+            {
+                ->
+                setOnclickLidter((new ).Onclicklistener() {
+
+                }));
+
+fun Nothing?.Onclicklistener() {
+
+
+
+
+
+         
+     
+ 
+
+  
+
+     
+
---
+++ app/src/main/java/com/example/sistemadelivery/activuty/startBtn.kt
@@ -0,0 +1,12 @@
+package com.example.sistemadelivery.activuty
+
+object startBtn {
+    fun findViewById(startBtn: Int) {
+
+    }
+
+    fun setOnclickLidter(any: Any) {
+
+    }
+
+}
